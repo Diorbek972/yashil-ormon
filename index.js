16e9f9e917df65d3d5874163c4d9d4002d3d5c09
diorbek <diorbeknorhuja@gmail.com>
2024-12-18 20:38:36 +0500

    jsni yoqtim

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// let x = "10";
-// let y = 5;
-// natija = x * y
-// console.log(natija)
+let x = "10";
+let y = 5;
+natija = x * y
+console.log(natija)
 
-// x - "10"
-// y - 5 
-// print(x * y)
+x - "10"
+y - 5 
+print(x * y)
